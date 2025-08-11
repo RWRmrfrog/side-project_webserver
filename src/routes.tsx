d516897b93aa412cb6fb8356f68033a8ff84af6d
Joshua Pechan <jashduck@gmail.com>
2025-08-11 12:39:50 -0400

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
-import { Routes, Route,  useLocation, useNavigate } from "react-router-dom"; //Navigate,
+import { Routes, Route, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import Home from "./pages/Home";
 import Devs from "./pages/Devs";
 import Users from "./pages/Users";
 import NoPage from "./pages/NoPage";
-import { useEffect } from "react"; //, type ReactNode
+import { useEffect, type ReactNode } from "react";
 import Layout from "./pages/Layout";
 
 function LoginPage() {
@@ -19,29 +19,33 @@ function LoginPage() {
       navigate(location.state?.from ?? "/", { replace: true });
     }
   }, [isAuthed, navigate, location.state]);
-  
+
   return <Authenticator initialState="signIn" />;
 }
 
-// function RequireAuth({ children }: { children: ReactNode  }) {
-//   const { route } = useAuthenticator((c) => [c.route]);
-//   const isAuthed = route === "authenticated";
-//   const location = useLocation();
-//   if (!isAuthed) return <Navigate to="/login" state={{ from: location.pathname }} replace />;
-//   return children;
-// }
+function RequireAuth({ children }: { children: ReactNode }) {
+  const { route } = useAuthenticator((c) => [c.route]);
+  const isAuthed = route === "authenticated";
+  const location = useLocation();
+  if (!isAuthed) return <Navigate to="/login" state={{ from: location.pathname }} replace />;
+  return children;
+}
 
 export default function AppRoutes() {
   return (
     <Routes>
-        <Route path="login" element={<LoginPage />} />
+      <Route path="login" element={<LoginPage />} />
+
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="devs" element={<Devs />} />
+        <Route path="users" element={<Users />} />
+        <Route path="*" element={<NoPage />} />
         
-        <Route element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="devs" element={<Devs />} />
-            <Route path="users" element={<Users />}/>
-            <Route path="*" element={<NoPage />} />
-        </Route>
+        <RequireAuth>
+          <Route></Route>
+        </RequireAuth>
+      </Route>
     </Routes>
   );
 }
